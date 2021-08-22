5976968e98f2240682f98bd50cbce573b88b236c
PitiViers <amoeba_wp@live.fr>
2021-08-22 07:13:09 +0200

    [TEST] ServerName usage

--- commands/servers/server-status.js
+++ commands/servers/server-status.js
@@ -82,13 +82,17 @@ module.exports = class MisServerInfoCommand extends Command {
     async run(message, args) {
         message.delete();
         let serverId = args.serverId
-        if (!serverId) { return message.say("You must specify a serverId to get info for.") }
+        if (!serverId) { return message.say("You must specify serverName or serverId to get info for.") }
 
         return new Promise(async (fulfill, reject) => {
 
             try {
                 fulfill(
                     await this.client.MiscreatedServers.getServer(message.guild.id, { server_id: serverId }).then(res => {
+                        return res
+                    })
+                    || 
+                    await this.client.MiscreatedServers.getServer(message.guild.id, { server_name: serverId }).then(res => {
                         return res
                     })
                 )
